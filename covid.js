55b5a42abe29a32edce59e35992972d1e72b2a8f
John Durso <johnkyrondurso@gmail.com>
2020-04-02 17:47:07 -0400

    new styles new db info new errythang

---
+++ covid.js
@@ -0,0 +1,164 @@
+const covidList = [
+ {
+   "Name": "Lloyd Blankfein",
+   "Company": "Goldman Sachs",
+   "Position": "Former CEO",
+   "Net": "$1 billion",
+   "Description": "Called for those with “lower risk to the disease” to return to work within a few weeks.",
+   "Quotes": "“Extreme measures to flatten the virus “curve” is sensible-for a time-to stretch out the strain on health infrastructure. But crushing the economy, jobs and morale is also a health issue-and beyond. Within a very few weeks let those with a lower risk to the disease return to work.”",
+   "Source": "https://twitter.com/lloydblankfein/status/1241907502662418437"
+ },
+ {
+   "Name": "Richard “Dick” Kovacevich",
+   "Company": "Wells Fargo",
+   "Position": "Former CEO",
+   "Net": "$1 Million - $5 Million (Approx.)",
+   "Description": "Called for healthy workers under 55 to return to work by April.",
+   "Quotes": "\"We'll gradually bring those people back and see what happens. Some of them will get sick, some may even die, I don't know.” \"Do you want to suffer more economically or take some risk that you'll get flu-like symptoms and a flu-like experience? Do you want to take an economic risk or a health risk? You get to choose.\"",
+   "Source": "https://www.businessinsider.com/richard-kovacevich-former-wells-fargo-ceo-work-die-coronavirus-pandemic-2020-3"
+ },
+ {
+   "Name": "José E. Almeida",
+   "Company": "Covidien",
+   "Position": "CEO",
+   "Net": "$16.6 million",
+   "Description": "The medical supply manufacturer Covidien bought the company Newport Medical Instruments, which was contracted by DHHS to manufacture cheaper portable ventilators, in 2012. Once acquired, Covidien then halted the project and exited the contract, supposedly to prevent a cheaper ventilator product from pushing their product out of the market. This ultimately contributed to the ventilator shortage during the COVID-19 pandemic.",
+   "Quotes": "",
+   "Source": "https://www.nytimes.com/2020/03/29/business/coronavirus-us-ventilator-shortage.html"
+ },
+ {
+   "Name": "Gary Cohn",
+   "Company": "",
+   "Position": "Former chief economic adviser, White House",
+   "Net": "$400 million",
+   "Description": "Recommended businesses reopen “incrementally” during the crisis.",
+   "Quotes": "\"We should be able to handle incremental economic activity in appropriate locations while not allowing it in other geographies.”",
+   "Source": "https://thehill.com/policy/finance/489310-gary-cohn-says-economy-could-be-reopened-on-incremental-basis"
+ },
+ {
+   "Name": "Larry Kudlow",
+   "Company": "",
+   "Position": "Chief economic adviser, White House",
+   "Net": "$2 million",
+   "Description": "Claimed virus was contained in February 2020.",
+   "Quotes": "“We have contained this. I won’t say [it’s] airtight, but it’s pretty close to airtight.”",
+   "Source": "https://www.cnbc.com/2020/02/25/larry-kudlow-says-us-has-contained-the-coronavirus-and-the-economy-is-holding-up-nicely.html"
+ },
+ {
+   "Name": "Suzanne Scott",
+   "Company": "Fox News",
+   "Position": "CEO",
+   "Net": "Unknown",
+   "Description": "Oversees Fox News, whose anchors have been criticized for downplaying the crisis and peddling misinformation.",
+   "Quotes": "",
+   "Source": "https://www.nytimes.com/2020/03/22/business/coronavirus-fox-news-lachlan-murdoch.html"
+ },
+ {
+   "Name": "",
+   "Company": "",
+   "Position": "",
+   "Net": "",
+   "Description": "",
+   "Quotes": "",
+   "Source": ""
+ },
+ {
+   "Name": "Kevin Hassett",
+   "Company": "",
+   "Position": "Economic adviser, White House",
+   "Net": "Unknown",
+   "Description": "Advised the president to reopen the economy during the pandemic.",
+   "Quotes": "“You really can’t shut down the global economy for six months and expect anything to continue.”",
+   "Source": "https://thehill.com/homenews/administration/488611-economist-kevin-hassett-returns-to-white-house-to-advise-trump-amid"
+ },
+ {
+   "Name": "Dan Patrick",
+   "Company": "",
+   "Position": "Lieutenant Governor, Texas",
+   "Net": "Unknown",
+   "Description": "Implied workers should be sacrificed to improve the economy.",
+   "Quotes": "“My message is, let’s get back to work. Let’s get back to living. Let’s be smart about it. And those of us who are 70-plus [years old], we’ll take care of ourselves. But don’t sacrifice the country.”",
+   "Source": "https://www.nbcnews.com/news/us-news/texas-lt-gov-dan-patrick-suggests-he-other-seniors-willing-n1167341"
+ },
+ {
+   "Name": "Tilman Fertitta",
+   "Company": "Landry’s, Inc.",
+   "Position": "CEO",
+   "Net": "$4.4 billion",
+   "Description": "Laid off 40,000 workers; called for businesses to reopen with limited capacity during the crisis.",
+   "Quotes": "“I think what we are doing with the shut-down is good but in a few weeks people will need to be around people. Otherwise you are going to go into an economic crisis that is going to take us years to dig ourselves out of.”",
+   "Source": "https://www.bloomberg.com/news/articles/2020-03-24/billionaire-fertitta-furloughs-40-000-warns-of-economic-crisis"
+ },
+ {
+   "Name": "Thomas J. Donohue",
+   "Company": "US Chamber of Commerce",
+   "Position": "CEO",
+   "Net": "$16 million",
+   "Description": "Lobbied against the deployment of the Defense Protection Act.",
+   "Quotes": "",
+   "Source": "https://thehill.com/business-a-lobbying/business-a-lobbying/490060-unions-urge-chamber-of-commerce-to-stop-lobbying"
+ },
+ {
+   "Name": "Jerry Falwell, Jr.",
+   "Company": "Liberty University",
+   "Position": "President",
+   "Net": "$100 million",
+   "Description": "Reopened Liberty University in the midst of the crisis.",
+   "Quotes": "“99 percent of [students] are not at the age to be at risk and they don't have conditions that put them at risk.\"",
+   "Source": "https://www.nytimes.com/2020/03/29/us/politics/coronavirus-liberty-university-falwell.html?smtyp=cur&smid=tw-nytpolitics https://www.nbcnews.com/think/opinion/trump-s-coronavirus-quarantine-rhetoric-fuels-dangerously-politicized-pandemic-ncna1170406"
+ },
+ {
+   "Name": "Joel Freedman",
+   "Company": "Broad Street Healthcare Properties",
+   "Position": "Unknown",
+   "Net": "Unknown",
+   "Description": "Bought and closed Hahnemann Hospital in Philadelphia. When city officials asked to use the hospital to treat COVID-19 patients, Freedman requested they “buy the hospital or lease it for almost $1 million a month, including utilities and other costs.”",
+   "Quotes": "",
+   "Source": "https://www.nytimes.com/2020/03/27/us/coronavirus-philadelphia-hahnemann-hospital.html"
+ },
+ {
+   "Name": "Daniel Turner",
+   "Company": "Power The Future",
+   "Position": "Founder and Executive Director",
+   "Net": "Unknown",
+   "Description": "Peddled the narrative that COVID-19 accomplishes the same ends as climate activism.",
+   "Quotes": "“Coronavirus is a glimpse of the long-term pain a Green New Deal and environmental radicalism would inflict on America.”",
+   "Source": "https://thefederalist.com/2020/03/13/how-the-wuhan-virus-is-accomplishing-the-green-new-deals-goals/"
+ },
+ {
+   "Name": "Rush Limbaugh",
+   "Company": "",
+   "Position": "Radio Personality",
+   "Net": "$500 million",
+   "Description": "Insisted that COVID-19 is just the “common cold” and railed against media “hype.”",
+   "Quotes": "\"It looks like the coronavirus is being weaponized as yet another element to bring down Donald Trump.”",
+   "Source": "https://www.politifact.com/factchecks/2020/feb/27/rush-limbaugh/fact-checking-rush-limbaughs-misleading-claim-new-/"
+ },
+ {
+   "Name": "Richard Epstein",
+   "Company": "Hoover Institution",
+   "Position": "Fellow",
+   "Net": "Unknown",
+   "Description": "Penned the article “Coronavirus Perspective,” which has circulated within the president’s administration, downplaying the danger of the virus.",
+   "Quotes": "“I fully understand the need for immediate responses to immediate threats, like fires, but not for crises that may last for three months or more.”",
+   "Source": "https://www.newyorker.com/news/q-and-a/the-contrarian-coronavirus-theory-that-informed-the-trump-administration https://www.hoover.org/research/coronavirus-isnt-pandemic"
+ },
+ {
+   "Name": "Michael Knowles",
+   "Company": "The Daily Wire",
+   "Position": "Political Commentator",
+   "Net": "Unknown",
+   "Description": "Consistently downplayed severity of COVID-19, called for reopening the economy.",
+   "Quotes": "“All right, enough of this. Time to start reopening.”",
+   "Source": "https://twitter.com/michaeljknowles/status/1244050622745214976"
+ },
+ {
+   "Name": "Apoorva Mehta",
+   "Company": "Instacart",
+   "Position": "CEO",
+   "Net": "$400 million",
+   "Description": "Mass hired workers to meet demand during the crisis without offering hazard pay, protective equipment, or paid sick time.",
+   "Quotes": "",
+   "Source": "https://www.npr.org/2020/03/30/823767492/amazon-instacart-grocery-delivery-workers-strike-for-coronavirus-protection-and-"
+ }
+]
--- publicfigures.js
+++ publicfigures.js
@@ -1,38 +1,58 @@
 const publicFigures = [
 	{
-		name: "Bill Boolin",
-		company: "McDonalds",
-		position: "CEO",
-		net: "$15.2B",
-		sources: "https://website.com/"
+		"name": "Bill Boolin",
+		"company": "McDonalds",
+		"position": "CEO",
+		"net": "$15.2B",
+		"sources": 
+		[
+			"https://website.com/",
+			""
+		]
 	},
 	{
-		name: "Tom Jones",
-		company: "Lucky Strike",
-		position: "CEO",
-		net: "$3.9B",
-		sources: "https://website.com/"
+		"name": "Jeff Bezons",
+		"company": "Amazon",
+		"position": "CEO",
+		"net": "$15.2B",
+		"sources": 
+		[
+			"https://website.com/",
+			""
+		]
 	},
 	{
-		name: "Jeff Bezos",
-		company: "Amazon",
-		position: "CEO",
-		net: "$150.4B",
-		sources: "https://website.com/"
+		"name": "Betty White",
+		"company": "Golden Girls",
+		"position": "CEO",
+		"net": "$15.2B",
+		"sources": 
+		[
+			"https://website.com/",
+			""
+		]
 	},
 	{
-		name: "Gary Williams",
-		company: "Dicks Sporting Goods",
-		position: "CEO",
-		net: "$1.7B",
-		sources: "https://website.com/"
+		"name": "Bill Gates",
+		"company": "Microsoft",
+		"position": "CEO",
+		"net": "$15.2B",
+		"sources":
+		[
+			"https://website.com/",
+			"https://website2.com/",
+		]
 	},
 	{
-		name: "Ronald Keith",
-		company: "Burger King",
-		position: "CFO",
-		net: "$583.8M",
-		sources: "https://website.com/"
+		"name": "Dick Cheney",
+		"company": "Haliburton",
+		"position": "CEO",
+		"net": "$15.2B",
+		"sources":
+		[
+			"https://website.com/",
+			"https://website2.com/",
+		]
 	},
 ];
 
@@ -43,16 +63,19 @@ function figuresTemplate(figure) {
     	<p><span>Company:</span> ${figure.company}</p>
     	<p><span>Position:</span> ${figure.position}</p>
     	<p><span>Net worth:</span> ${figure.net}</p>
-    	<p class="source"><span>Sources:</span> <a href="${figure.sources}">${figure.sources}</a></p>
+    	<div class="sources">
+	    	<p class="source"><span>Sources:</span></p>
+	    		<ul>
+	    			<li><a href="${figure.sources[0]}">${figure.sources[0]}</a></li>
+	    			<li><a href="${figure.sources[1]}">${figure.sources[1]}</a></li>
+	    		</ul>
+	    </div>
     </div>
   `;
 }
 
 
 document.getElementById("publicFigures").innerHTML = `
-	<h1>Working with COVID-19</h1>
-	<p>Listed below are the public figures forcing their workers to continue to work after testing positive for COVID-19</p>
-	<h3>Total count = ${publicFigures.length}</h3>
 	${publicFigures.map(figuresTemplate).join('')}
 
 `;
